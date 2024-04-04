888d5f4c5d8c3cb6362483c887298693f8790e50
BOUSSAG Youness <youness.boussag10@gmail.com>
2024-04-04 14:11:23 +0200

    WIP : probleme d'ajout d'input texte

--- library/bdd_infoger.php
+++ library/bdd_infoger.php
@@ -7,7 +7,7 @@ class BDD_infoger extends BDDManager{
     public function Connexion()
     {
         //Utilisation de parent:: car BDD_infoger est hérité des méthodes BDDManager (son père)
-        parent::ConnexionBDD("192.168.100.210", "dev_web", "123+aze", "infoger");
+        parent::ConnexionBDD("localhost", "infoger_user1", "123456+azerty", "infoger");
     }
     
     public function isConnected()
@@ -44,65 +44,82 @@ class BDD_infoger extends BDDManager{
 
 
     // Méthode pour Lister les clients qui ont le même service//
-    public function SQL_lister_service_client($Id_client)
-    {
-    return parent::executerRequete("SELECT c.* FROM client AS c INNER JOIN service AS s ON c.Id_client = s.Id_service");
+    public function SQL_lister_service_client($Id_client){
+        return parent::executerRequete("SELECT c.* FROM client AS c INNER JOIN service AS s ON c.Id_client = s.Id_service");
     }
 
   
-      // Méthode pour faire passer l'id du status de 1 à 2 ou de 2 à 1 
-      public function SQL_switch_status_service_client($Id_client, $Id_service, $id_nom_status)
-      {
-          if(is_null($id_nom_status)){
+    // Méthode pour faire passer l'id du status de 1 à 2 ou de 2 à 1 
+    public function SQL_switch_status_service_client($Id_client, $Id_service, $id_nom_status, $id_parametre)
+    {
+        if(is_null($id_nom_status)){
               $id_nom_status = 1;
-          }
+        }
           
-          if ($id_nom_status == 1){
+        if ($id_nom_status == 1){
               $id_nom_status = 2;
-          }
-          else{
+        }
+        else{
               $id_nom_status = 1;
-          }   
+        }   
           
-          $tab = array(
+        $tab = array(
               'Id_nom_status' => $id_nom_status
               );// tableau
   
           
-          parent::modifierDonnees("status"," Id_client=".$Id_client." AND Id_service = ".$Id_service, $tab);
-      }
+        parent::modifierDonnees("status"," Id_client=".$Id_client." AND Id_service = ".$Id_service, $tab);
+
+      if (empty($id_parametre) && is_null($id_parametre)){
+        $tab2 = array(
+            'Id_client' => $Id_client, 
+            'Id_service' => $Id_service,
+            'Id_nom_parametre'=> '1',
+            );// tableau
+
+        parent::insererDonnees("parametre",$tab2);
+        
+        $tab3 = array(
+            'Id_client' => $Id_client, 
+            'Id_service' => $Id_service,
+            'Id_nom_parametre'=> '2',
+            );// tableau
+
+        parent::insererDonnees("parametre",$tab2);
+        }
+    }
   
-      // Méthode pour modifier les paramètres des services
-      public function SQL_modifier_parametre_service($Id_parametre, $valeur_parametre)
-      {
+    // Méthode pour modifier les paramètres des services
+    public function SQL_modifier_parametre_service($Id_parametre, $valeur_parametre)
+    {
           $tab = array(
               'valeur_parametre' => $valeur_parametre
               ); // tableau
           
           parent::modifierDonnees("parametre","Id_parametre = ".$Id_parametre, $tab);
-      }
+    }
   
   
   
-      //Lister les services//
-      public function SQL_lister_services()
-      {
+    //Lister les services//
+    public function SQL_lister_services()
+    {
       return parent::executerRequete("SELECT nom_service, service.Id_service AS n_service, disponible FROM service;");
-  }
+    }
   
-      //Méthode pour lister les paramètres
-      public function SQL_lister_parametres($num_client, $num_service)
-      {
+    //Méthode pour lister les paramètres
+    public function SQL_lister_parametres($num_client, $num_service)
+    {
       return parent::executerRequete("SELECT nom, valeur_parametre, Id_parametre FROM parametre JOIN nom_parametre ON parametre.Id_nom_parametre = nom_parametre.Id_nom_parametre WHERE Id_client=".$num_client." and Id_service=".$num_service.";");
-      }
+    }
   
-      //Méthode pour lister les status des services du clients
-      public function SQL_lister_status($num_client)
+    //Méthode pour lister les status des services du clients
+    public function SQL_lister_status($num_client)
       {
       return parent::executerRequete("SELECT nom, status.Id_nom_status AS n_status , Id_service AS n_service FROM status JOIN nom_status ON status.Id_nom_status = nom_status.Id_nom_status WHERE Id_client=".$num_client.";");
-      }
+    }
   
-      public function SQL_ajouter_status($Id_nom_status,  $Id_service, $Id_client)
+    public function SQL_ajouter_status($Id_nom_status,  $Id_service, $Id_client)
       {
           $tab = array(
               'Id_nom_status' => $Id_nom_status,
@@ -111,9 +128,9 @@ class BDD_infoger extends BDDManager{
               ); // tableau de donnée
   
           parent::insererDonnees("status",$tab);
-      }
+    }
   
-      public function SQL_ajouter_parametre_virtualhost($Id_service, $Id_nom_parametre, $Id_client)
+    public function SQL_ajouter_parametre_virtualhost($Id_service, $Id_nom_parametre, $Id_client)
       {
           $tab = array(
               'Id_service' => $Id_service,
@@ -122,11 +139,11 @@ class BDD_infoger extends BDDManager{
               ); // tableau de donnée
   
           parent::insererDonnees("parametre",$tab);
-      }
+    }
   
   
-      //Modification d'informations du client//
-      public function SQL_modifier_client($Id_client, $nom_entreprise, $adresse_entreprise, $nom_referent, $mail_referent, $tel_referent)
+    //Modification d'informations du client//
+    public function SQL_modifier_client($Id_client, $nom_entreprise, $adresse_entreprise, $nom_referent, $mail_referent, $tel_referent)
       {
           
           $tab = array(
@@ -138,7 +155,7 @@ class BDD_infoger extends BDDManager{
               ); // tableau
   
           parent::modifierDonnees("client","Id_client=".$Id_client, $tab);
-      }
+    }
   
 }
 ?>
